c7f7f5f9a3d6e2a040d55a78b4e93f4574467405
zzdeagle545zz <deagles545@mail.com>
2016-03-07 14:09:22 -0800

    Signed-off-by: zzdeagle545zz <deagles545@mail.com>

---
+++ Lecture/Lecture_Pointers/main.cpp
@@ -0,0 +1,78 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on March 7, 2016, 12:59 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+/*
+ * The "&" to get the address
+ * int num=99
+ * &num gives the address
+ * Pointers holds he address
+ * 
+ * Arrays are passed by reference
+ * like by reference
+ * 
+ * pointer
+ * defined *(name) 
+ * *pointer is the value
+ * pointer is the address
+ * *pointer=&value
+ * pointer=value
+ * 
+ * *pointer = value will change the variable
+ * 
+ * Arrays are pointers but pointers are not arrays
+ * 
+ * vals[2]
+ * pointer[2]
+ * *(vals+2)
+ * *(pointer+2)
+ * 
+ * if(!pointer) if it has a value
+ * if(pointer==pointer) same address
+ * if(*pointer) same value
+ * 
+ * void function(int *,int *)
+ * void function (int *x,int *y) as function parameters
+ * function(needs address)
+ * 
+ * pointers to a constant
+ * const int *array doesnt change data
+ * 
+ * int * const ptr = address doesnt change
+ * const int * const ptr = address niether can change with the pointer
+ * 
+ * Dynamic memory Allocation
+ * new operator
+ * 
+ * int *dptr;
+ * dptr=new int;
+ * 
+ * delete dptr
+ * delete [] dptr
+ * 
+ * can return pointers from function
+ * int* functon can return Arrays
+ * can allocate memory in function and return that memory
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+    return 0;
+}
+
